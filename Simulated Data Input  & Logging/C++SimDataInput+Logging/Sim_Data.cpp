4f75c5b33009f71e81c42353d7b3e0dcc0be8539
hsingh837 <harshal.singh2006@gmail.com>
2025-10-13 00:49:10 -0400

    Initiating C++ Sim Program, beginning with resolving directory issues across platforms

--- Simulated Data Input  & Logging/C++SimDataInput+Logging/Sim_Data.cpp
+++ Simulated Data Input  & Logging/C++SimDataInput+Logging/Sim_Data.cpp
@@ -1,1 +1,32 @@
 //same thing as simulated data.C to build a skeleton of what we are looking to do.
+//A goal with this is to ensure no directory issues occur across MacOS, Windows, and Linux systems.
+
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <filesystem>
+#include <string>
+#include <iostream>
+
+#if defined (_WIN32)
+    #include <windows.h>
+#elif defined(__APPLE__)
+    #include <mach-o/dyld.h>
+#elif defined(__LINUX__)
+    #include <unistd.h>
+#endif
+
+namespace fs = std::filesystem;
+
+static fs::path getExecutableDir() {
+#if defined(_WIN32)
+    wchar_t buf[MAX_PATH];
+    DWORD len = GetModuleFileNameW(nullptr, buf, MAX_PATH);
+    if (len == 0 || len == MAX_PATH) throw std::runtime_error("GetModuleFileNameW failed");
+    return fs::path(buf).parent_path();
+
+//    #elif defined(__APPLE__)
+//uint32_tsize = 0;
+
+#endif
+}
