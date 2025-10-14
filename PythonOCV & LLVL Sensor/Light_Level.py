d485370496b3dc81e486b3f6bd14f6ac9f0e2349
hsingh837 <harshal.singh2006@gmail.com>
2025-10-14 00:01:56 -0400

    json files to complete dev environment for openCV with C++, as well as writing the first iteration of the Recording.cpp program

---
+++ PythonOCV & LLVL Sensor/Light_Level.py
@@ -0,0 +1,3 @@
+#This is our actual light level sensor code. 
+#We will measure light level by number of white pixels seen in the footage,
+#and our CSV will be marked with colored cells at certain timestamps when changes in light level are detected.
